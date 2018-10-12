c934dfa44b88e110f2f2d8f3127f8c5892c344fe
Michael Bovee <michael.j.bovee@gmail.com>
2018-10-12 10:29:31 -0600

    added ddl scripts

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -5,4 +5,75 @@ ALTER DATABASE mbovee CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 DROP TABLE IF EXISTS commment;
 DROP TABLE IF EXISTS `like`;
 DROP TABLE IF EXISTS photo;
-DROP TABLE IF EXISTS profile
+DROP TABLE IF EXISTS profile;
+
+-- create comment table
+
+CREATE TABLE comment (
+	-- primary key
+	commentId BINARY(16) NOT NULL,
+	-- foreign keys
+	commentPhotoId BINARY(16) NOT NULL,
+	commentProfileId BINARY(16) NOT NULL,
+	-- attributes
+	commentDate DATETIME(6) NOT NULL,
+	-- indices for foreign keys
+	INDEX(commentPhotoId),
+	INDEX(commentProfileId),
+	-- create foreign key relations
+	FOREIGN KEY(commentPhotoId) REFERENCES photo(photoId),
+	FOREIGN KEY(commentProfileId) REFERENCES profile(profileId),
+	-- create primary key
+	PRIMARY KEY(commentId)
+);
+
+-- create like table
+
+CREATE TABLE `like` (
+	-- foreign keys
+	likePhotoId BINARY(16) NOT NULL,
+	likeProfileId BINARY(16) NOT NULL,
+	-- attributes
+	likeDate DATETIME(6) NOT NULL,
+	-- indices for foreign keys
+	INDEX(likePhotoId),
+	INDEX(likeProfileId),
+	-- create foreign key relations
+	FOREIGN KEY(likePhotoId) REFERENCES photo(photoId),
+	FOREIGN KEY(likeProfileId) REFERENCES profile(profileId),
+	-- create primary key
+	PRIMARY KEY(likePhotoId, likeProfileId)
+);
+
+CREATE TABLE photo (
+	-- primary key
+	photoId BINARY(16) NOT NULL,
+	-- foreign keys
+	photoProfileId BINARY(16) NOT NULL,
+	-- attributes
+	photoCopyright VARCHAR(128),
+	photoFormat VARCHAR(8),
+	photoPath VARCHAR(128) NOT NULL,
+	photoSize VARCHAR(16) NOT NULL,
+	photoTitle VARCHAR(64) NOT NULL,
+	-- create foreign key relations
+	FOREIGN KEY(photoProfileId) REFERENCES profile(photoProfileId),
+	-- create primary key
+	PRIMARY KEY(photoId)
+);
+
+CREATE TABLE profile (
+	--primary key
+	profileId BINARY(16) NOT NULL,
+	-- attributes
+	profileDisplayName VARCHAR(32) NOT NULL,
+	profileEmail VARCHAR(128) NOT NULL,
+	profileHash CHAR(9) NOT NULL,
+	profileRealName VARCHAR(32) NOT NULL,
+	profileWebAddress VARCHAR(128),
+	-- prevent duplicate data
+	UNIQUE(profileDisplayName),
+	UNIQUE(profileEmail),
+	-- primary key
+	PRIMARY KEY(profileId)
+);
