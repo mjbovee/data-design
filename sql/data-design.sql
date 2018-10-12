2e8f9a152cb9659a30f74f241944f393bcba3646
Michael Bovee <michael.j.bovee@gmail.com>
2018-10-12 09:56:23 -0600

    changed "user profile" to "profile"

---
+++ sql/data-design.sql
@@ -0,0 +1,8 @@
+ALTER DATABASE mbovee CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+-- drop tables if they exist
+
+DROP TABLE IF EXISTS commment;
+DROP TABLE IF EXISTS `like`;
+DROP TABLE IF EXISTS photo;
+DROP TABLE IF EXISTS profile
