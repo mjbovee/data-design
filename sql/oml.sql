732a1c8ee3f3f8c9832d1c13d69ff61b035c754d
Michael Bovee <michael.j.bovee@gmail.com>
2018-10-15 14:32:12 -0600

    added in update, delete, select, inner join statements

--- sql/oml.sql
+++ sql/oml.sql
@@ -4,4 +4,11 @@ INSERT INTO photo(photoId, photoProfileId, photoPath, photoSize, photoTitle) VAL
 
 INSERT INTO comment(commentId, commentPhotoId, commentProfileId, commentDate) VALUES(unhex("86988389e4a142c18b282e4045a38e81"), unhex("a000656ef8e24517b5d9a7af552805ef"), unhex("95ad0914b2a343608d88414bf9d08091"), "10-12-12");
 
-UPDATE profile SET profileDisplayName = "michaeljbovee91", profileRealName = "Michael J Bovee" where profileId = unhex("95ad0914b2a343608d88414bf9d08091");
+UPDATE profile SET profileDisplayName = "michaeljbovee91", profileRealName = "Michael J Bovee" WHERE profileId = unhex("95ad0914b2a343608d88414bf9d08091");
+
+DELETE FROM comment WHERE commentId = unhex("86988389e4a142c18b282e4045a38e81");
+
+SELECT photoId, photoProfileId, photoPath, photoSize, photoTitle FROM photo WHERE photoId = unhex("a000656ef8e24517b5d9a7af552805ef");
+
+SELECT profile.profileId, profile.profileDisplayName, photo.photoId, photo.photoTitle FROM profile INNER JOIN photo ON photo.photoProfileId = profile.profileId WHERE photoId = unhex("a000656ef8e24517b5d9a7af552805ef");
+
