5434582ea2d495a6954ed16c8d13ad19e02407ba
Michael Bovee <michael.j.bovee@gmail.com>
2018-10-17 14:01:25 -0600

    added php directory, beginning of php class

---
+++ php/Profile.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 10/17/18
+ * Time: 1:22 PM
+ */
+
+/**This is an abbreviated example of what is potentially stored about a user on a site like Flickr. This is a top-level entity and holds keys about other entities, such as photographs and comments.
+ *
+ * @author Michael Bovee <michael.j.bovee@gmail.com>
+ * @version 1.0.0
+ */
+
+class Profile {
+	use ValidateUuid;
+	/**
+	 * id for this profile
+	 * @var Uuid $profileId
+	 */
+	private $profileId;
+	/**
+	 * display name for this profile
+	 * @var string $profileDisplayname
+	 */
+	private $profileDisplayName;
+	/**
+	 * email address for this profile
+	 * @var string $profileEmail
+	 */
+	private $profileEmail;
+	/**
+	 * hash for this profile
+	 * @var $profileHash
+	 */
+	private $profileHash;
+	/**
+	 * real name attributed to user of this profile
+	 * @var string $profileRealName
+	 */
+	private $profileRealName;
+	/**
+	 * web address associated with this profile
+	 * @var string $profileWebAddress
+	 */
+	private $profileWebAddress;
+	/**
+	 * accessor method for profile id
+	 *
+	 * @return Uuid value of profile id - this will be null if new profile
+	 */
+	public function getProfileId(): Uuid {
+		return($this->profileId);
+	}
+	/**
+	 * mutator method for profile id
+	 *
+	 * @param
+	 */
+	public function setProfileId() {
+
+	}
+	/**
+	 *accessor method for profile display name
+	 *
+	 * @return string value of profile display name
+	 */
+	public function getProfileDisplayName() : ?string {
+		return ($this->profileDisplayName);
+	}
+	/**
+	 * mutator method for profile display name
+	 *
+	 * @param string $profileDisplayName
+	 * @throws \InvalidArgumentException if display name is not a string
+	 *
+	 */
+}
