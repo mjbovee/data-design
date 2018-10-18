971a8562ea49df6c124bb9782c836d9fada3019e
Michael Bovee <michael.j.bovee@gmail.com>
2018-10-17 21:30:05 -0600

    added profile hash accessor and mutator

--- php/Classes/Foo.php
+++ php/Classes/Foo.php
@@ -15,22 +15,22 @@
 class Foo {
 	use ValidateUuid;
 	/**
-	 * id for this profile
+	 * id for this profile; this is the primary key
 	 * @var Uuid $profileId
 	 */
 	private $profileId;
 	/**
-	 * display name for this profile
+	 * display name for this profile; this is a unique attribute
 	 * @var string $profileDisplayname
 	 */
 	private $profileDisplayName;
 	/**
-	 * email address for this profile
+	 * email address for this profile; this is a unique attribute
 	 * @var string $profileEmail
 	 */
 	private $profileEmail;
 	/**
-	 * hash for this profile
+	 * hash for the password associated with this profile
 	 * @var $profileHash
 	 */
 	private $profileHash;
@@ -71,9 +71,9 @@ class Foo {
 	/**
 	 * mutator method for profile display name
 	 *
-	 * @param string value of profileDisplayName
+	 * @param string $profileDisplayName value of new profile display name
 	 * @throws \InvalidArgumentException if display name is not a string or if it's insecure
-	 * @throws \RangeException if display name is > 32 characters
+	 * @throws \RangeException if length of display name is greater than 32 characters
 	 * @throws \TypeError if display name is not a string
 	 */
 	public function setProfileDisplayName(string $newProfileDisplayName) : void {
@@ -81,12 +81,13 @@ class Foo {
 		$newProfileDisplayName = filter_var($newProfileDisplayName,FILTER_SANITIZE_STRING);
 		// verify that the new display name is a string
 		if($newProfileDisplayName === false) {
-			throw new \InvalidArgumentException("Your new profile display name is not a string or is insecure");
+			throw(new \InvalidArgumentException("Your new profile display name is not a string or is insecure"));
 		}
 		// verify that the new display name is not too long
 		if(strlen($newProfileDisplayName) > 32) {
-			throw new \RangeException("Your new profile display name is too long");
+			throw(new \RangeException("Your new profile display name is too long"));
 		}
+		// store the new profile display name
 		$this->profileDisplayName = $newProfileDisplayName;
 	}
 	/**
@@ -100,30 +101,56 @@ class Foo {
 	/**
 	 * mutator method for profile email
 	 *
-	 * @para string value of profile email
+	 * @param string $newProfileEmail new value of profile email
 	 * @throws \InvalidArgumentException if email is not a valid email address or if it's insecure
-	 * @throws \RangeException if email is >
-	 *
-	 */
-	/**
-	 * @param string $profileEmail
+	 * @throws \RangeException if email string length is greater than 128 characters
 	 */
 	public function setProfileEmail(string $newProfileEmail): void {
 		$newProfileEmail = trim($newProfileEmail);
 		$newProfileEmail = filter_var($newProfileEmail, FILTER_VALIDATE_EMAIL);
 		// verify email is a valid address
 		if($newProfileEmail === false) {
-			throw new \InvalidArgumentException("That is not a valid email address");
+			throw(new \InvalidArgumentException("That is not a valid email address"));
 		}
 		// verify email is not too long
 		if(strlen($newProfileEmail > 128)) {
-			throw new \RangeException("That email address is too long");
+			throw(new \RangeException("That email address is too long"));
 		}
+		// store the new email
 		$this->profileEmail = $newProfileEmail;
 	}
-
 	/**
-	 *accessor method for profile real name
+	 * accessor method for profileHash
+	 *
+	 * @return string value of hash
+	 */
+	public function getProfileHash() : string {
+		return $this->profileHash;
+	}
+	/**
+	 * mutator method for profile hash
+	 *
+	 * @param string $newProfileHash  new string value for profile hash
+	 * @throws \InvalidArgumentException if hash is not secure
+	 * @throws \RangeException if length of hash is not equal to 97 characters
+	 * @throws \TypeError if hash is not a string
+	 */
+	public function setProfileHash(string $newProfileHash): void {
+		// check hash to make sure it is formatted correctly
+		$newProfileHash = trim($newProfileHash);
+		if($newProfileHash === false) {
+			throw(new \InvalidArgumentException("Profile hash is not secure"));
+		}
+		// Argon hash???
+		// check string length
+		if(strlen($newProfileHash) !== 97) {
+			throw(new \RangeException("Profile hash must be 97 characters"));
+		}
+		// store new hash
+		$this->profileHash = $newProfileHash;
+	}
+	/**
+	 * accessor method for profile real name
 	 *
 	 * @return string value of profile real name
 	 */
@@ -133,21 +160,23 @@ class Foo {
 	/**
 	 * mutator method for profile real name
 	 *
-	 * @param string value of profile real name
+	 * @param string $newProfileRealName new value of profile real name
 	 * @throws \InvalidArgumentException if display name is not a string or if it's insecure
-	 * @throws \RangeException if display name is > 32 characters
+	 * @throws \RangeException if length of display name is greater than 32 characters
 	 * @throws \TypeError if display name is not a string
 	 */
-	public function setProfileRealName(string $newProfileRalName) : void {
+	public function setProfileRealName(string $newProfileRealName) : void {
 		$newProfileRealName = trim($newProfileRealName);
-		$newProfileRealName = filter_var($newProfileRealName,FILTER_SANITIZE_STRING);
+		$newProfileRealName = filter_var($newProfileRealName, FILTER_SANITIZE_STRING);
 		// verify that the new real name is a string
 		if($newProfileRealName === false) {
-			throw new \InvalidArgumentException("Your new profile real name is not a string or is insecure");
+			throw(new \InvalidArgumentException("Your new profile real name is not a string or is insecure"));
 		}
 		// verify that the new real name is not too long
 		if(strlen($newProfileRealName) > 32) {
-			throw new \RangeException("Your new profile real name is too long");
+			throw(new \RangeException("Your new profile real name is too long"));
 		}
+		// store the new real profile name
 		$this->profileRealName = $newProfileRealName;
+	}
 }
