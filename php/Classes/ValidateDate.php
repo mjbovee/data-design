19922586a7af1b59338a96e476a9e1f1f41f6b7c
Michael Bovee <michael.j.bovee@gmail.com>
2018-10-18 10:51:23 -0600

    added validate date and validate uuid trait files; added docblock to validate uuid file

---
+++ php/Classes/ValidateDate.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 10/18/18
+ * Time: 10:27 AM
+ */
---
+++ php/Classes/ValidateUuid.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 10/18/18
+ * Time: 10:35 AM
+ */
+/**
+ * Trait to validate Uuid
+ * Trait will accept Uuids in the following formats:
+ * 	1. Binary string
+ * 	2. Human readable string
+ * 	3. Ramsey\Uuid object
+ *
+ * @author Michael Bovee <michael.j.bovee@gmail.com> - referencing trait originally authored by Dylan McDonald
+ * @documentation https://bootcamp-coders.cnm.edu/class-materials/object-oriented/object-oriented-php.php
+ */
+trait ValidateUuid {
+
+}
