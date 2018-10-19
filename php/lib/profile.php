4dd5231c4863bd44eec1e7c39e4654cd77e323a4
Michael Bovee <michael.j.bovee@gmail.com>
2018-10-19 11:52:17 -0600

    changed prof values - pretty inconsequential

--- php/lib/profile.php
+++ php/lib/profile.php
@@ -8,6 +8,6 @@
 namespace Michaelbovee\DataDesign;
 require_once ("../Classes/autoload.php");
 
-$instance = new Profile("82f90cb6-9bf2-4387-8449-7b6a4c80a9ef", "MikeB", "mikeb@email.com", "7TplU3FHKll7QFqClUbYhuChCYA5mnoj7BsPvXn36w0M1gcd0bZ5tpcWdAPpO3wJvMoOzzHuVw6NC7Ot8DmqTncUNR8pJuY27", "Mike Bove", "http://www.google.com");
+$instance = new Profile("82f90cb6-9bf2-4387-8449-7b6a4c80a9ef", "MikeB", "michael.j.bovee@gmail.com", "7TplU3FHKll7QFqClUbYhuChCYA5mnoj7BsPvXn36w0M1gcd0bZ5tpcWdAPpO3wJvMoOzzHuVw6NC7Ot8DmqTncUNR8pJuY27", "Mike Bovee", "http://www.google.com");
 
 var_dump($instance);
