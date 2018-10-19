4321f3c109dfb63300a1dae25bea6fdecf1a73ae
Michael Bovee <michael.j.bovee@gmail.com>
2018-10-19 09:15:43 -0600

    changed namespace

--- php/lib/profile.php
+++ php/lib/profile.php
@@ -5,8 +5,8 @@
  * Date: 10/18/18
  * Time: 3:53 PM
  */
-namespace Foo\Bar\DataDesign;
+namespace Michaelbovee\DataDesign;
 
-$instance = new \Foo\Bar\DataDesign\Profile("bac2128c571e4ede963d031d2198eca7", "MikeB", "mikeb@email.com", "ptXXJHyRgGM4DAMaN6N8cwXN2VaBIUzOZ4YNZakjBu1hk6jne5g9HYh6CxIypfqU97a6S8FLXpX5r8NGFUTRB22hoN4JmaPl4", "Mike Bove", "mikebovee.com");
+$instance = new Profile("bac2128c571e4ede963d031d2198eca7", "MikeB", "mikeb@email.com", "ptXXJHyRgGM4DAMaN6N8cwXN2VaBIUzOZ4YNZakjBu1hk6jne5g9HYh6CxIypfqU97a6S8FLXpX5r8NGFUTRB22hoN4JmaPl4", "Mike Bove", "mikebovee.com");
 
 var_dump($instance);
