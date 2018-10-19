c76a8849618c731d42851f07ec6ac019940f74dc
Michael Bovee <michael.j.bovee@gmail.com>
2018-10-19 07:58:09 -0600

    added profile.php file to lib directory

---
+++ php/lib/profile.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 10/18/18
+ * Time: 3:53 PM
+ */
+namespace Foo\Bar\DataDesign;
+
+$instance = new \Foo\Bar\DataDesign\Profile("bac2128c571e4ede963d031d2198eca7", "MikeB", "mikeb@email.com", "ptXXJHyRgGM4DAMaN6N8cwXN2VaBIUzOZ4YNZakjBu1hk6jne5g9HYh6CxIypfqU97a6S8FLXpX5r8NGFUTRB22hoN4JmaPl4", "Mike Bove", "mikebovee.com");
+
+var_dump($instance);
